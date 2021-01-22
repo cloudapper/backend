11c012d72a296ea984ce4f62d0b9a87f3cd6b082
Hendrik Ohrdes <hohrdes@gmail.com>
2021-01-22 16:10:28 +0100

    added automatic timestamp

--- src/main/java/de/f73/adlbackend/persistence/models/CarDataEntity.java
+++ src/main/java/de/f73/adlbackend/persistence/models/CarDataEntity.java
@@ -2,6 +2,7 @@ package de.f73.adlbackend.persistence.models;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class CarDataEntity {
     @Id
     private String id;
     private String fin;
+    @CreatedDate
     private LocalDateTime timestamp;
     private String[] data;    
 }
