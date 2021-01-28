a2c1b9f08e5937c7cee18b35ef3098dc0fa1db5f
Robert Rudolph <searchforsense@gmail.com>
2021-01-28 11:20:38 +0100

    fixed wrong changes httpsecurity filters

--- src/main/java/de/f73/adlbackend/SecurityConfig.java
+++ src/main/java/de/f73/adlbackend/SecurityConfig.java
@@ -3,6 +3,7 @@ package de.f73.adlbackend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -22,7 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     {
         httpSecurity
                 .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated().antMatchers("/").permitAll()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
